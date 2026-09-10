5d9f2a77d6f215166d1aff50ed619559bf6009cd
mouadsbaer <mouadsbaer@gmail.com>
2026-09-10 11:42:00 +0100

    Feat : ajout de estAutorise avec le type char

--- src/main/java/ma/youcode/lineperm/access/ControleAcces.java
+++ src/main/java/ma/youcode/lineperm/access/ControleAcces.java
@@ -12,4 +12,22 @@ public class ControleAcces {
         }
         return user.getLogin().equals(fichier.getProprietaire());
     }
+
+    // une seule categorie : proprietaire -> bloc gauche, sinon -> bloc droite
+    public static boolean estAutorise(User user, FichierProtege fichier, char droit) {
+        if (user == null || fichier == null) {
+            return false;
+        }
+        boolean proprio = estProprietaire(user, fichier);
+        if (droit == 'r') {
+            return proprio ? fichier.isrProprio() : fichier.isrAutres();
+        }
+        if (droit == 'w') {
+            return proprio ? fichier.iswProprio() : fichier.iswAutres();
+        }
+        if (droit == 'd') {
+            return proprio ? fichier.isdProprio() : fichier.isdAutres();
+        }
+        return false;
+    }
 }
